64c1c0a2d207d1e1e46ca96ebac3176f30a5bbee
Serkan Isler <serkanisler88@gmail.com>
2022-03-19 23:25:23 +0300

    Sort işlemi için düzenleme yapıldı.

--- prep_casa.py
+++ prep_casa.py
@@ -43,7 +43,7 @@ def prep_casa_csv():
             counter += 1     
 
     df_casalar = pd.DataFrame.from_dict(casalar, orient="index")
-    df_casalar.sort_index(axis = 0)
+    df_casalar = df_casalar.sort_index()
     print(df_casalar)
     df_casalar.to_csv(y+".csv", sep=";", index=True)
     print(f"Dosya {y+'.csv'} adıyla oluşturuldu.")
